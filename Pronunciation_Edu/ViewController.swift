55c2b4daabfbf4050a2c38cc5488af75a53abed2
skrth00 <skrth00@naver.com>
2017-03-16 18:15:34 +0900

    프로젝트 생성 후 커밋

---
+++ Pronunciation_Edu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pronunciation_Edu
+//
+//  Created by 전한경 on 2017. 3. 16..
+//  Copyright © 2017년 jeon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
